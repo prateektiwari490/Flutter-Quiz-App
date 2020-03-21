4175cbbecc77f0baf2ff4e3846fb51e8eaf23e0f
Prateek Tiwari <prateektiwari490@gmail.com>
2020-03-22 00:36:56 +0530

    this is my Quiz App using Flutter
    These 5 files are inside lib folder

---
+++ lib/main.dart
@@ -0,0 +1,86 @@
+import 'package:first_app/result.dart';
+import 'package:flutter/material.dart';
+import './quiz.dart';
+
+void main() => runApp(_MyApp());
+
+class _MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<_MyApp> {
+  var _questionIndex = 0;
+  var _totalScore = 0;
+
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
+  final _questions = const [
+    {
+      'questionText': 'What\'s your favorite color?',
+      'answer': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 6},
+        {'text': 'Green', 'score': 3},
+        {'text': 'White', 'score': 1}
+      ],
+    },
+    {
+      'questionText': 'What\'s your favorite animal',
+      'answer': [
+        {'text': 'Rabbit', 'score': 3},
+        {'text': 'Snake', 'score': 11},
+        {'text': 'Elephant', 'score': 5},
+        {'text': 'Lion', 'score': 9}
+      ],
+    },
+    {
+      'questionText': 'Who\'s is your favorite instructor',
+      'answer': [
+        {'text': 'Max', 'score': 1},
+        {'text': 'Max', 'score': 1},
+        {'text': 'Max', 'score': 1},
+        {'text': 'Max', 'score': 1}
+      ],
+    },
+  ];
+
+  void _answerQuestion(int score) {
+    // var aBool = true;
+    _totalScore = _totalScore + score;
+    if (_questionIndex < _questions.length) {
+      print('we have more _questions!!');
+    }
+
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+    print(_questionIndex);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions,
+              )
+            : Result(_totalScore, _resetQuiz),
+      ),
+    );
+  }
+}
